83351cb150126eceb7c3a226578e144f66887850
Honksponk <96245211+Honksponk@users.noreply.github.com>
2022-04-22 11:28:57 +0200

    rückwärtsAusgabe.java

---
+++ src/com/company/rueckwaertsAusgabe.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class rueckwaertsAusgabe {
+    public static String gebeRueckwaerts(String name){
+        char[] arr =  name.toCharArray();
+        char[] arr1 = new char[arr.length];
+
+        for(int i=0; i<arr.length; i++){
+            arr1[i] = arr[arr.length-1-i];
+        }
+        String result = new String(arr1);
+
+        return result;
+    }
+}
