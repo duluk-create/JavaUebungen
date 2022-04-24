33a85f970e326d73f36c8d57024d16198ed10327
Honksponk <96245211+Honksponk@users.noreply.github.com>
2022-04-24 14:23:52 +0200

    findeMinimaleDifferenz.java

---
+++ src/com/company/findeMinimaleDifferenz.java
@@ -0,0 +1,29 @@
+package com.company;
+
+public class findeMinimaleDifferenz {
+
+
+    public static String minDistanz(int[] arr) {
+        int diff = Integer.MAX_VALUE; //setze diff auf unendlich
+        int index = 0;
+        for (int i = 0; i < arr.length - 1; i++) {
+
+            if (arr[i] > arr[i + 1]) {
+                if (diff > arr[i] - arr[i + 1]) {
+                    diff = arr[i] - arr[i + 1];
+                    index = i;
+                }
+            } else {
+                if (diff > arr[i + 1] - arr[i]) {
+                    diff = arr[i + 1] - arr[i];
+                    index = i;
+                }
+
+
+            }
+        }
+        return "Die kleinste Differenz beträgt " + diff + " und befindet sich bei Index " + index;
+
+
+    }
+}
