2f9ea7b91171e968f22a6ab61a1b86f3eee232f9
duluk <96245211+duluk-create@users.noreply.github.com>
2022-05-17 10:14:22 +0200

    Baut einen String nach Semikolons auseinander

---
+++ src/com/company/nameSplit.java
@@ -0,0 +1,25 @@
+package com.company;
+
+import java.util.ArrayList;
+
+public class nameSplit {
+
+    public static ArrayList nameSplit(String name){
+        char[] arr = name.toCharArray();
+        ArrayList<String> output = new ArrayList<>();
+        String speicher = "";
+        for(int i=0; i<name.length();i++){
+            if(arr[i] != ';'){
+                speicher = speicher.concat(Character.toString(arr[i]));
+            }
+            else{
+                output.add(speicher);
+                speicher = "";
+            }
+        }
+        if(!speicher.isEmpty()){
+            output.add(speicher);
+        }
+        return output;
+    }
+}
