cefc906d3421f5059c569f975274e8e16927f60e
duluk <96245211+duluk-create@users.noreply.github.com>
2022-06-06 18:48:19 +0200

    Verarbeitet eine Benutzereingabe und gibt Sternchen aus

---
+++ src/com/company/SternAusgabe.java
@@ -0,0 +1,24 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class SternAusgabe {
+    public static String sterneSehen(){
+
+        Scanner keyboard=new Scanner(System.in);
+
+        System.out.println("Wieviele Sterne möchtest du sehen?");
+
+        int zahl = keyboard.nextInt();
+
+        String output = "";
+
+        while(zahl>0) {
+            output = output.concat("*");
+            zahl--;
+        }
+
+        return output;
+
+    }
+}
